125c812b846c92b5178af81d1ffd0f84c32fb965
Maxx Rodriguez <maxxrdrgz@gmail.com>
2025-05-10 09:48:56 -0700

    working header

---
+++ colony-app/src/components/header.svelte
@@ -0,0 +1,26 @@
+<header class="">
+  <div class="navbar bg-base-100 shadow-sm">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl">Colony</a>
+    </div>
+    <div class="flex-2 flex justifiy-center">
+      <ul class="menu menu-horizontal px-1 flex justify-between w-full">
+        <li><a>Search</a></li>
+        <li>
+          <details>
+            <summary>Uploads</summary>
+            <ul class="bg-base-100 rounded-t-none p-2">
+              <li><a>Link 1</a></li>
+              <li><a>Link 2</a></li>
+            </ul>
+          </details>
+        </li>
+        <li><a>Downloads</a></li>
+        <li><a>Colonies</a></li>
+        <li><a>Wallet</a></li>
+        <li><a>Configuration</a></li>
+      </ul>
+    </div>
+    <div class="flex-1"></div>  <!-- Added this empty div for balance -->
+  </div>
+</header>
