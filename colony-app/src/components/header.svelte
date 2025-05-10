5125432b1092aacf8573991157cf1728fa7c4ce7
Maxx Rodriguez <maxxrdrgz@gmail.com>
2025-05-10 12:22:07 -0700

    updated header with some new routes

--- colony-app/src/components/header.svelte
+++ colony-app/src/components/header.svelte
@@ -1,11 +1,14 @@
 <header class="">
   <div class="navbar bg-base-100 shadow-sm">
     <div class="flex-1">
-      <a class="btn btn-ghost text-xl">Colony</a>
+      <a href="/screens/app-info" class="btn btn-ghost text-xl">
+        <span><img src="/logo.svg" class="logo svelte-kit" alt="Colony Logo" width="24" height="24"/></span>
+        Colony
+      </a>
     </div>
-    <div class="flex-2 flex justifiy-center">
+    <div class="flex-3 flex justifiy-center">
       <ul class="menu menu-horizontal px-1 flex justify-between w-full">
-        <li><a>Search</a></li>
+        <li><a href="/screens/search">Search</a></li>
         <li>
           <details>
             <summary>Uploads</summary>
@@ -18,7 +21,7 @@
         <li><a>Downloads</a></li>
         <li><a>Colonies</a></li>
         <li><a>Wallet</a></li>
-        <li><a>Configuration</a></li>
+        <li><a href="/welcome-intro">Configuration</a></li>
       </ul>
     </div>
     <div class="flex-1"></div>  <!-- Added this empty div for balance -->
