974fd781a9683bbc1fc1f932921140259dfa7a6f
Maxx Rodriguez <maxxrdrgz@gmail.com>
2025-05-10 09:48:41 -0700

    moved layout.ts to layout.svelte

---
+++ colony-app/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  // Tauri doesn't have a Node.js server to do proper SSR
+  // so we will use adapter-static to prerender the app (SSG)
+  // See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
+  export const prerender = true;
+  export const ssr = false;
+
+  import "../app.css";
+  import Header from "../components/header.svelte";
+  import Footer from "../components/footer.svelte";
+</script>
+
+<Header></Header>
+
+<main>
+  <slot></slot>
+</main>
