eb9d1230e627397bdc285f1720f698b0f4248e96
Maxx Rodriguez <maxxrdrgz@gmail.com>
2025-05-10 21:38:49 -0700

    figured out how to import rust code

--- colony-app/src-tauri/src/lib.rs
+++ colony-app/src-tauri/src/lib.rs
@@ -1,14 +1,22 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use colony::config::generate_seed_phrase;
+use serde::Serialize;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+fn get_new_seed_phrase() -> Result<colony::config::SeedPhrase, String> {
+    Ok(generate_seed_phrase())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, get_new_seed_phrase])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
