2dd072d83309071b00e0ee75f21bd5ef9c749943
Maxx Rodriguez <maxxrdrgz@gmail.com>
2025-06-01 17:14:28 -0700

    updated header with new links

--- colony-app/src/components/header.svelte
+++ colony-app/src/components/header.svelte
@@ -9,18 +9,19 @@
     <div class="flex-3 flex justifiy-center">
       <ul class="menu menu-horizontal px-1 flex justify-between w-full">
         <li><a href="/screens/search">Search</a></li>
-        <li><a href="/screens/pod-management/uploads">Pod Management</a></li>
+        <!-- <li><a href="/screens/pod-management/uploads">Pod Management</a></li> -->
         <li>
           <details>
             <summary>Pod Management (dropdown)</summary>
-            <ul class="bg-base-100 rounded-t-none p-2">
+            <ul class="bg-base-100 rounded-t-none p-2" style="z-index: 1000;">
+              <li><a href="/screens/pod-management/your-pods">Your Pods</a></li>
               <li><a>Uploads</a></li>
-              <li><a>Downloads</a></li>
+              <li><a href="/screens/pod-management/downloads">Downloads</a></li>
             </ul>
           </details>
         </li>
         <li><a>Colonies</a></li>
-        <li><a>Wallet</a></li>
+        <li><a href="/screens/wallet">Wallet</a></li>
         <li><a href="/screens/configuration">Configuration</a></li>
         <li><a href="/welcome-intro">Intro</a></li>
       </ul>
