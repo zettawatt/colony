be3214325009012a768803f2a88e1840427df9bd
Maxx Rodriguez <maxxrdrgz@gmail.com>
2025-06-09 16:17:37 -0700

    introduced drawer component

---
+++ src/components/drawer.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let drawerId: string = "my-drawer-2"; // Allow custom IDs if needed
+</script>
+
+<div class="drawer drawer-open" style="padding-top:1vh;">
+  <input id={drawerId} type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content flex flex-col">
+    <slot name="main" />
+  </div>
+  <div class="drawer-side" style="height: 90vh;">
+    <label for={drawerId} aria-label="close sidebar" class="drawer-overlay"></label>
+    <slot name="sidebar" />
+  </div>
+</div>
