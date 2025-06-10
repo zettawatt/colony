ea7bfbc46526d00a86a84f66a48ce62d9b5858c7
Maxx Rodriguez <maxxrdrgz@gmail.com>
2025-06-10 14:19:23 -0700

    updated seed phrase component added bip39 js library

--- src/components/seedPhrase.svelte
+++ src/components/seedPhrase.svelte
@@ -1,41 +1,36 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/core";
+  import * as bip39 from '@scure/bip39';
+  import { wordlist } from '@scure/bip39/wordlists/english';
 
-  let name = $state("");
-  let greetMsg = $state("");
+  let seedWords: string[] = Array(12).fill("");
 
-  export async function generate(event: Event) {
-    event.preventDefault();
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    let seed = await invoke("get_new_seed_phrase");
-    console.log(seed)
+  export function generateNewSeedPhrase() {
+    const mn = bip39.generateMnemonic(wordlist);
+    seedWords = mn.split(" ");
+    return mn;
   }
 </script>
 
 <div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
   <table class="table">
-    <!-- head -->
     <tbody>
-      <!-- row 1 -->
       <tr>
-        <td><input type="text" class="input input-primary" value="temp1" id="1" /></td>
-        <td><input type="text" class="input input-primary" value="temp2" id="2" /></td>
-        <td><input type="text" class="input input-primary" value="temp3" id="3" /></td>
-        <td><input type="text" class="input input-primary" value="temp4" id="4" /></td>
+        <td><input type="text" class="input input-primary" bind:value={seedWords[0]} id="1" /></td>
+        <td><input type="text" class="input input-primary" bind:value={seedWords[1]} id="2" /></td>
+        <td><input type="text" class="input input-primary" bind:value={seedWords[2]} id="3" /></td>
+        <td><input type="text" class="input input-primary" bind:value={seedWords[3]} id="4" /></td>
       </tr>
-      <!-- row 2 -->
       <tr>
-        <td><input type="text" class="input input-primary" value="temp5" id="5" /></td>
-        <td><input type="text" class="input input-primary" value="temp6" id="6" /></td>
-        <td><input type="text" class="input input-primary" value="temp7" id="7" /></td>
-        <td><input type="text" class="input input-primary" value="temp8" id="8" /></td>
+        <td><input type="text" class="input input-primary" bind:value={seedWords[4]} id="5" /></td>
+        <td><input type="text" class="input input-primary" bind:value={seedWords[5]} id="6" /></td>
+        <td><input type="text" class="input input-primary" bind:value={seedWords[6]} id="7" /></td>
+        <td><input type="text" class="input input-primary" bind:value={seedWords[7]} id="8" /></td>
       </tr>
-      <!-- row 3 -->
       <tr>
-        <td><input type="text" class="input input-primary" value="temp9" id="9" /></td>
-        <td><input type="text" class="input input-primary" value="temp10" id="10" /></td>
-        <td><input type="text" class="input input-primary" value="temp11" id="11" /></td>
-        <td><input type="text" class="input input-primary" value="temp12" id="12" /></td>
+        <td><input type="text" class="input input-primary" bind:value={seedWords[8]} id="9" /></td>
+        <td><input type="text" class="input input-primary" bind:value={seedWords[9]} id="10" /></td>
+        <td><input type="text" class="input input-primary" bind:value={seedWords[10]} id="11" /></td>
+        <td><input type="text" class="input input-primary" bind:value={seedWords[11]} id="12" /></td>
       </tr>
     </tbody>
   </table>
