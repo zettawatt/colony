1a31ab05c3a93070592968fa5ef0f2574ac6b4d1
Maxx Rodriguez <maxxrdrgz@gmail.com>
2025-06-11 15:46:17 -0700

    updated perstore to use fileobj class

--- src/stores/persistantStorage.ts
+++ src/stores/persistantStorage.ts
@@ -1,21 +1,26 @@
 import { LazyStore } from "@tauri-apps/plugin-store";
 import { appDataDir } from '@tauri-apps/api/path';
+import { FileObj } from "../classes/FileObj";
 
-const STORE_NAME = "colony-app-state.json"
+const STORE_NAME = "colony-app-state.json";
 const STORE_VERSION = 1;
 let store: LazyStore | null = null;
 
+
 export async function initStore() {
-    const dir = await appDataDir();
-    store = new LazyStore(`${dir}/${STORE_NAME}`, {autoSave: true});
-    await store.set('__version', STORE_VERSION);
-    return store;
+  const dir = await appDataDir();
+  store = new LazyStore(`${dir}/${STORE_NAME}`, { autoSave: true });
+  await store.set('__version', STORE_VERSION);
+  await store.set('uploadedFiles', {});
+  await store.set('downloadFiles', {});
+  await store.set('podCache', {});
+  return store;
 }
 
 export async function getStore() {
   if (!store) {
     const dir = await appDataDir();
-    store = new LazyStore(`${dir}/${STORE_NAME}`, {autoSave: true});
+    store = new LazyStore(`${dir}/${STORE_NAME}`, { autoSave: true });
   }
   return store;
 }
@@ -27,32 +32,62 @@ export async function eraseStore() {
 }
 
 export async function setUserCompletedIntro(has: boolean) {
-    const store = await getStore();
-    store.set("hasUserCompletedIntro", has);
+  const store = await getStore();
+  await store.set("hasUserCompletedIntro", has);
 }
 
-export async function getUserCompletedIntro(): Promise<Boolean | undefined> {
-    const store = await getStore();
-    return store.get("hasUserCompletedIntro");
+export async function getUserCompletedIntro(): Promise<boolean | undefined> {
+  const store = await getStore();
+  return store.get("hasUserCompletedIntro");
 }
 
-export async function addUploadedFile() {
-
+export async function addUploadedFileObj(fileObj: FileObj) {
+  const store = await getStore();
+  const uploadedFiles = await getUploadedFiles();
+  if(uploadedFiles && fileObj.autonomiAddress) {
+    uploadedFiles[fileObj.autonomiAddress] = fileObj.toJSON()
+    await store.set('uploadedFiles', uploadedFiles);
+  }
 }
 
-export async function addDownloadedFile() {
-  
+export async function getUploadedFileObj(uuid: string) {
+  const store = await getStore();
+  return store.get('uploadedFiles.' + uuid);
 }
 
-export const ps = {
+export async function getUploadedFiles(): Promise<Record<string, unknown> | undefined> {
+  const store = await getStore();
+  return (await store.get('uploadedFiles'));
+}
+
+export async function addDownloadedFile(fileObj: FileObj) {
+  const store = await getStore();
+  await store.set('downloadFiles.' + fileObj.uuid, fileObj);
+}
+
+export async function getDownloadedFileObj(uuid: string) {
+  const store = await getStore();
+  return store.get('downloadFiles.' + uuid);
+}
+
+export async function getDownloadedFiles(): Promise<Record<string, FileObj>> {
+  const store = await getStore();
+  return (await store.get('downloadFiles')) as Record<string, FileObj>;
+}
+
+const ps = {
   initStore,
   getStore,
   eraseStore,
   setUserCompletedIntro,
   getUserCompletedIntro,
-  addUploadedFile,
+  addUploadedFileObj,
+  getUploadedFileObj,
+  getUploadedFiles,
   addDownloadedFile,
-}
+  getDownloadedFileObj,
+  getDownloadedFiles
+};
 
 export default ps;
 
