90252b6c5c98e9776b65d11ff25a50e3152c95e5
Maxx Rodriguez <maxxrdrgz@gmail.com>
2025-06-10 18:54:12 -0700

    on app startup, init store check if user has already completed the intro from store

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,46 @@
-<script>
+<script lang="ts">
+    import { onMount } from "svelte";
+
   // import Welcome from "../pages/welcome.svelte";
   import Intro from "./welcome-intro/+page.svelte";
+  import ps from "../stores/persistantStorage";
+
+
+  let hasUserCompletedIntro: Boolean | undefined = undefined;
+  let loading = true;
+
+  async function checkIfUserIsNew() {
+    try {
+      console.log("hs")
+      hasUserCompletedIntro = await ps.getUserCompletedIntro();
+      console.log(hasUserCompletedIntro);
+      if (hasUserCompletedIntro === undefined) {
+        await ps.setUserCompletedIntro(false);
+        hasUserCompletedIntro = false;
+      } else if (hasUserCompletedIntro) {
+        window.location.href = '/screens/search';
+      }
+    } catch (e) {
+      console.log(e)
+    } finally {
+      loading = false;
+    }
+  }
+
+  onMount(async () => {
+    await ps.initStore();
+    await checkIfUserIsNew();
+  })
 
 </script>
 
 <main>
   <div class="">
-    <Intro/>
+    {#if !loading && hasUserCompletedIntro === false}
+      <div class="">
+        <Intro/>
+      </div>
+    {/if}
   </div>
 </main>
 
