d2be9cd5a00b6c99030729e4686005348411dd76
Maxx Rodriguez <maxxrdrgz@gmail.com>
2025-06-23 16:49:20 -0700

    working tabulator svelte component

---
+++ src/components/tabulator.svelte
@@ -0,0 +1,20 @@
+<script>
+  import {TabulatorFull as Tabulator} from 'tabulator-tables';
+  import {onMount} from 'svelte';
+  import 'tabulator-tables/dist/css/tabulator.min.css';
+
+  export let data, columns;
+
+  let tableComponent;
+
+  onMount(() => {
+    new Tabulator(tableComponent, {
+      data: data, //link data to table
+      reactiveData:true, //enable data reactivity
+      columns: columns, //define table columns
+      layout: 'fitDataStretch'
+    });
+  });
+</script>
+
+<div bind:this={tableComponent}></div>
