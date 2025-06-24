904483ca5edd30be026e571ba9c457b002afc9d7
Maxx Rodriguez <maxxrdrgz@gmail.com>
2025-06-23 21:19:45 -0700

    table util, needs to be reorganized

---
+++ src/utils/searchColumns.js
@@ -0,0 +1,20 @@
+export const searchColumns = [
+  {
+    title: "Name",
+    field: "name",
+    width: 500,
+    formatter: "plaintext"
+  },
+  {
+    title: "Size", 
+    field: "size",
+    width: 70,
+    hozAlign: "right"
+  },
+  {
+    title: "Address",
+    field: "address",
+    width: 200,
+    formatter: "plaintext"
+  },
+];
