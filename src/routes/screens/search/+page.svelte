5f2b2ab1f3a2999af3e5c1000a133bc2a2497265
Maxx Rodriguez <maxxrdrgz@gmail.com>
2025-06-23 21:20:13 -0700

    working on new search screen implementation

--- src/routes/screens/search/+page.svelte
+++ src/routes/screens/search/+page.svelte
@@ -4,7 +4,25 @@
   import { torrentsData } from '../../../components/testData';
   import { searchColumns } from '../../../utils/searchColumns';
   import { testDataSearch } from '../../../components/testDataSearch';
-  
+  import { invoke } from "@tauri-apps/api/core";
+
+  async function simpleSearch() {
+    try {
+      const request = {
+        query: {
+          // Put your search parameters here, e.g.:
+            "type": "text",
+            "text": "maxx",
+            "limit": 10
+        },
+      };
+      // const request = {query: "beg"}
+      const results = await invoke('search', { request });
+      console.log(results)
+    } catch (error) {
+      console.log(error)
+    }
+  }
   
   // Function to update data (useful for real-time updates)
   // function updateTorrentData(newData) {
@@ -20,9 +38,9 @@
     </div>
 
     <input type="radio" name="my_tabs_2" class="tab" aria-label="Search" checked="checked" />
-    <div class="tab-content border-base-300 p-10 pt-3">
+    <div class="tab-content border-base-300 bg-base-100 p-10 pt-3">
       <div class="row mb-3">
-        <label class="input mr-1">
+        <label class="input mr-2">
           <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <g
               stroke-linejoin="round"
@@ -37,7 +55,7 @@
           </svg>
           <input type="search" required placeholder="Search"/>
         </label>
-        <button class="btn btn-sof btn-warning">Search</button>
+        <button class="btn btn-sof btn-warning" onclick={()=>simpleSearch()}>Search</button>
       </div>
       <TabulatorTable data={testDataSearch} columns={searchColumns} />
     </div>
