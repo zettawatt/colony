11fc49109fe75cec1244b9b9b414f341e4f9b8a1
Maxx Rodriguez <maxxrdrgz@gmail.com>
2025-06-27 20:16:01 -0700

    wrote code to parse sparql query results search table still not working tho

--- src/routes/screens/search/+page.svelte
+++ src/routes/screens/search/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script type="ts">
   import TabulatorTable from '../../../components/tabulator.svelte';
   import { torrentsColumns } from '../../../components/testCols';
   import { torrentsData } from '../../../components/testData';
@@ -6,21 +6,73 @@
   import { testDataSearch } from '../../../components/testDataSearch';
   import { invoke } from "@tauri-apps/api/core";
 
+  let searchInput = $state("");
+  let tableSearchResults = $state([])
+
   async function simpleSearch() {
     try {
+      if (searchInput === "") return;
       const request = {
         query: {
           // Put your search parameters here, e.g.:
             "type": "text",
-            "text": "maxx",
-            "limit": 10
+            "text": searchInput,
+            "limit": 200
         },
       };
       // const request = {query: "beg"}
-      const results = await invoke('search', { request });
-      console.log(results)
+      const response = await invoke('search', { request });
+      const parsedResults = parseSparqlResults(response.results)
+      console.log(parsedResults)
+      tableSearchResults = parsedResults;
     } catch (error) {
       console.log(error)
+    }
+  }
+
+  function parseSparqlResults(results) {
+    try {
+      const aggregate = {};
+      const searchResults = {
+        metadata: {
+          pods_found: results.pods_found,
+          result_count: results.result_count,
+          search_timestamp: results.search_timestamp
+        },
+        variables: results.sparql_results.head.vars,
+        bindings: results.sparql_results.results.bindings,
+      }
+
+      for (let i = 0; i < searchResults.bindings.length; i++) {
+        const binding = searchResults.bindings[i];
+        if (!(binding.subject.value in aggregate)){
+          aggregate[binding.subject.value] = {
+            pod: binding.graph.value.startsWith("ant://") 
+              ? binding.graph.value.slice(6) : binding.graph.value,
+            address: binding.subject.value.startsWith("ant://") 
+              ? binding.subject.value.slice(6) : binding.subject.value,
+            depth: binding.depth.value
+          };
+        }
+        switch (binding.predicate.value) {
+          case 'http://schema.org/name':
+            aggregate[binding.subject.value].name = binding.object.value;
+            break;
+          case 'http://schema.org/description':
+            aggregate[binding.subject.value].description = binding.object.value;
+            break;
+          case 'http://schema.org/contentSize':
+            aggregate[binding.subject.value].size = Number(binding.object.value);
+            break;
+          case 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type':
+            aggregate[binding.subject.value].type = binding.object.value;
+            break;
+        }
+      }
+      return Object.values(aggregate);
+    } catch (error) {
+      console.trace(error)
+      return;
     }
   }
   
@@ -53,11 +105,11 @@
               <path d="m21 21-4.3-4.3"></path>
             </g>
           </svg>
-          <input type="search" required placeholder="Search"/>
+          <input type="search" required placeholder="Search" bind:value={searchInput}/>
         </label>
         <button class="btn btn-sof btn-warning" onclick={()=>simpleSearch()}>Search</button>
       </div>
-      <TabulatorTable data={testDataSearch} columns={searchColumns} />
+      <TabulatorTable data={tableSearchResults} columns={searchColumns} />
     </div>
   </div>
   <!-- <TabulatorTable data={torrentsData} columns={torrentsColumns} /> -->
