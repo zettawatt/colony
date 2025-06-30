8b0ae5c99594809e9109d96d59ab7edbade4abf1
Maxx Rodriguez <maxxrdrgz@gmail.com>
2025-06-30 16:24:04 -0700

    added test mock data for the status table

---
+++ src/utils/search/statusTestData.js
@@ -0,0 +1,51 @@
+export const statusTestData = [
+  {
+    name: "File_A.pdf",
+    status: "Complete",
+    size: "2.5 MB",
+    timer: "00:01:23",
+    startedDate: "2024-06-27 10:03:00"
+  },
+  {
+    name: "Data_Backup_2024.zip",
+    status: "Errored",
+    size: "750 MB",
+    timer: "00:00:42",
+    startedDate: "2024-06-27 09:54:18"
+  },
+  {
+    name: "Vacation_Photos.tar",
+    status: "Uploading",
+    size: "1.2 GB",
+    timer: "00:03:11",
+    startedDate: "2024-06-27 10:15:00"
+  },
+  {
+    name: "Presentation.pptx",
+    status: "Downloading",
+    size: "54 MB",
+    timer: "00:00:58",
+    startedDate: "2024-06-27 10:10:09"
+  },
+  {
+    name: "Draft_Notes.txt",
+    status: "Cancelled",
+    size: "15 KB",
+    timer: "00:00:05",
+    startedDate: "2024-06-27 09:59:33"
+  },
+  {
+    name: "Draft_Notes.txt",
+    status: "Pending",
+    size: "15 KB",
+    timer: "00:00:05",
+    startedDate: "2024-06-27 09:59:33"
+  },
+  {
+    name: "Research_Data.xlsx",
+    status: "Not Yet Uploaded",
+    size: "330 KB",
+    timer: "-",
+    startedDate: "2024-06-27 10:20:21"
+  }
+];
