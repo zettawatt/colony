0c8fbffc56dfe886d03493e2a1af84775da310f0
Maxx Rodriguez <maxxrdrgz@gmail.com>
2025-07-02 17:44:18 -0700

    updated login to actually auth the user

--- src/components/login.svelte
+++ src/components/login.svelte
@@ -1,33 +1,64 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import { setPassword } from '../utils/password/session';
+  import { getPassword, setPassword } from '../utils/password/session';
+  import { invoke } from '@tauri-apps/api/core';
+  import { tick } from 'svelte';
   let loginModal: HTMLDialogElement;
-  let password;
+  let password: string;
+  let wasPasswordInvalid = false;
+  let checkingAuth = false;
 
   onMount(() => {
-    // Open the modal when the component is mounted
-    loginModal?.showModal();
+    if (loginModal) {
+      loginModal.showModal();
+
+      // Prevent closing via Escape key
+      loginModal.addEventListener('cancel', (e) => {
+        e.preventDefault();
+      });
+    }
   });
 
-  function handleClose() {
-    console.log(password)
+  async function handleClose() {
     if (!password) return;
-    setPassword(password);
-    loginModal?.close();
+    checkingAuth = true;
+    wasPasswordInvalid = false;
+    await tick();
+    try {
+      await invoke("open_keystore", { password: password });
+      setPassword(password);
+      loginModal?.close(); 
+    } catch (error) {
+      console.trace(error);
+      wasPasswordInvalid = true;
+      checkingAuth = false;
+    }
   }
 
-  function authUser() {
-    // TODO write the auth check to open keystore
+  async function handleKeydown(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      await handleClose();
+    }
   }
+
 </script>
 
-<dialog id="my_modal_1" class="modal" bind:this={loginModal}>
+<dialog id="my_modal_1" class="modal backdrop-blur-sm" bind:this={loginModal} onkeydown={handleKeydown}>
   <div class="modal-box">
     <h3 class="text-lg font-bold">Login</h3>
     <p class="pt-4">Your password is needed for write access to your pod storage.</p>
-    <input type="password" placeholder="Password" class="input input-bordered w-full my-4" bind:value={password}/>
+    <input type="password" placeholder="Password" class="input w-full my-4 input-{wasPasswordInvalid ? "error": "bordered"}" bind:value={password}/>
+    {#if wasPasswordInvalid}
+      <p class="text-red-500">Password was invalid</p>
+    {/if}
     <div class="modal-action">
-      <button class="btn" on:click={handleClose}>Authenticate</button>
+      <button class="btn btn-error" onclick={()=>{handleClose()}}>
+        {#if checkingAuth}
+          <span class="loading loading-spinner"></span>
+        {:else}
+          Authenticate
+        {/if}
+      </button>
     </div>
   </div>
 </dialog>
