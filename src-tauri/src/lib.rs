91a59f0e123aa3695d8e10d79538e6e8a62c5188
Chuck McClish <github.vanish213@passmail.net>
2025-07-05 00:34:29 -0400

    Updated to colonylib v0.4.4. Added the Tauri shell feature for future addition of dweb sidecar

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1768,6 +1768,7 @@ pub fn run() {
     };
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_notification::init())
