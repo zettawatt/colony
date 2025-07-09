068d5a3dff3fd9d0ad76371b00e031446a3d5d08
Maxx Rodriguez <maxxrdrgz@gmail.com>
2025-07-09 09:42:40 -0700

    removed unused code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,14 +5,6 @@
   import Intro from "./welcome-intro/+page.svelte";
   import UserIntro from "./user-intro/+page.svelte";
   import ps from "../stores/persistantStorage";
-  import { invoke } from "@tauri-apps/api/core";
-  import { getPrimaryWallet } from "../utils/wallet/getPrimaryWallet";
-
-  let name = $state("");
-  let greetMsg = $state("");
-  let statusMessage = $state("");
-  let isLoading = $state(false);
-
 
   let hasUserCompletedIntro: Boolean | undefined = $state(undefined);
   let loading = $state(true);
@@ -37,51 +29,9 @@
     }
   }
 
-  async function initializeAutonomiClient() {
-    // const walletKey = prompt("Enter wallet key:");
-    // if (!walletKey) {
-    //   statusMessage = "Wallet key is required";
-    //   return;
-    // }
-
-    // const walletKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-    // const walletAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
-    // const walletKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-
-    isLoading = true;
-    statusMessage = "Initializing Autonomi client...";
-
-    try {
-      const primaryWallet = await getPrimaryWallet();
-      const walletKey = primaryWallet?.privateKey
-      console.log("primaryWallet", primaryWallet)
-      const result = await invoke("initialize_autonomi_client", { walletKey });
-      statusMessage = `Success: ${result}`;
-    } catch (error) {
-      statusMessage = `Error: ${error}`;
-    } finally {
-      isLoading = false;
-    }
-    console.log(statusMessage)
-  }
-  
-  async function initPodManager(wasUserNew: boolean) {
-    try {
-      await invoke("initialize_datastore");
-      // if (!wasUserNew) {await invoke("open_keystore", { password: "" });}
-      await invoke("initialize_graph");
-      // const result = await invoke("initialize_pod_manager");/
-
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
   onMount(async () => {
     console.log("here maxx")
     const wasUserNew = await checkIfUserIsNew();
-    // await initializeAutonomiClient();
-    // await initPodManager(wasUserNew);
     if (!wasUserNew) {
       window.location.href = '/screens/search';
     }
