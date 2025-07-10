184e412877e02e954354636f421b765078b566a5
Maxx Rodriguez <maxxrdrgz@gmail.com>
2025-07-10 12:15:22 -0700

    keep track of theme reactively in svelte store

--- src/components/tabulator.svelte
+++ src/components/tabulator.svelte
@@ -3,13 +3,14 @@
   import { TabulatorFull as Tabulator } from 'tabulator-tables';
   import { getCurrentWindow } from "@tauri-apps/api/window";
   import { DateTime } from "luxon";
+  import { globalTheme } from '../stores/globals';
 
   export let columns, data, rowMenu, initialSort;
 
   let tableComponent;
   let tabulatorInstance;
   let unlisten;
-  let currentTheme = 'light'; // default
+  let currentTheme = $globalTheme; // default
 
   function switchTabulatorTheme(theme) {
     const link = document.getElementById('tabulator-theme');
@@ -22,9 +23,8 @@
     }
   }
 
-
   onMount(async () => {
-
+    // const currentTheme = await getCurrentWindow().theme();
     // Create Tabulator instance
     tabulatorInstance = new Tabulator(tableComponent, {
       columns,
@@ -42,6 +42,7 @@
 
     // Listen for theme changes from Tauri
     unlisten = await getCurrentWindow().onThemeChanged(({ payload: theme }) => {
+      console.log("theme changed", theme)
       switchTabulatorTheme(theme);
     });
   });
@@ -53,6 +54,10 @@
 
   $: if (tabulatorInstance && Array.isArray(data)) {
     tabulatorInstance.replaceData(data);
+  }
+
+  $: if (typeof $globalTheme === 'string') {
+    switchTabulatorTheme($globalTheme);
   }
 </script>
 
--- src/stores/globals.ts
+++ src/stores/globals.ts
@@ -2,3 +2,4 @@ import { writable } from "svelte/store";
 
 export const podsSyncing = writable(false);
 export const allPodsUploading = writable(false);
+export const globalTheme = writable("light");
