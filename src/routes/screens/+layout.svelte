7224bb9a0320463db4193380947fe5cdb04794eb
Maxx Rodriguez <maxxrdrgz@gmail.com>
2025-07-13 16:04:06 -0700

    added semicolon to layout.svelte

--- src/routes/screens/+layout.svelte
+++ src/routes/screens/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-  import "../../app.css"
+  import "../../app.css";
   import Header from "../../components/header.svelte";
   import Footer from "../../components/footer.svelte";
 </script>
