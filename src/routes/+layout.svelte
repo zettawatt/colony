5843770b68421e7fc0a4f501dbc136295a196e49
Maxx Rodriguez <maxxrdrgz@gmail.com>
2025-07-13 21:48:15 -0700

    include forward logging code on initial startup page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,27 @@
 <script>
   import "../app.css";
-    import Toast from '../components/toast.svelte';
+  import Toast from '../components/toast.svelte';
+
+  import { attachConsole, trace, debug, info, warn, error } from '@tauri-apps/plugin-log';
+
+  // Optional: show Rust logs in DevTools
+  // attachConsole();
+
+  // This forwards browser logs to Tauri
+  function forwardConsole(fnName, logger) {
+    const original = console[fnName];
+    console[fnName] = (...args) => {
+      original(...args); // still show logs in DevTools
+      logger(args.map(String).join(' ')); // send logs to Tauri
+    };
+  }
+
+  // Hook up all the console methods to Tauri logger
+  forwardConsole('log', trace);
+  forwardConsole('debug', debug);
+  forwardConsole('info', info);
+  forwardConsole('warn', warn);
+  forwardConsole('error', error);
 </script>
 
 <div class="bg-base-200 dark:bg-base-300">
