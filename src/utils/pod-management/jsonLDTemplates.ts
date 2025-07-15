b47fa35ccd72b408bce781de98b3d88e772e9115
Maxx Rodriguez <maxxrdrgz@gmail.com>
2025-07-14 20:19:14 -0700

    updated templates to be more inline with colony

--- src/utils/pod-management/jsonLDTemplates.ts
+++ src/utils/pod-management/jsonLDTemplates.ts
@@ -2,7 +2,8 @@ export const templates = {
   Book: {
     "@context": { "schema": "http://schema.org/" },
     "@type": "schema:Book",
-    "schema:name": "Example Book Title",
+    "schema:name": "book-4fa2ad.pdf",
+    "schema:alternateName": "Example Book Title",
     "schema:author": "Author Name",
     "schema:contentSize": "563214",
     "schema:datePublished": "2020",
@@ -17,7 +18,8 @@ export const templates = {
   Movie: {
     "@context": { "schema": "http://schema.org/" },
     "@type": "schema:Movie",
-    "schema:name": "Example Movie Title",
+    "schema:name": "movie-e50b13.mp4",
+    "schema:alternateName": "Example Movie Title",
     "schema:director": "Director Name",
     "schema:actor": "Lead Actor",
     "schema:contentSize": "785412934",
@@ -32,7 +34,8 @@ export const templates = {
   MusicRecording: {
     "@context": { "schema": "http://schema.org/" },
     "@type": "schema:MusicRecording",
-    "schema:name": "Track Title",
+    "schema:name": "track-8dedc9.mp3",
+    "schema:alternateName": "Track Title",
     "schema:byArtist": "Artist Name",
     "schema:inAlbum": "Album Name",
     "schema:contentSize": "4159823",
@@ -47,7 +50,8 @@ export const templates = {
   TVSeries: {
     "@context": { "schema": "http://schema.org/" },
     "@type": "schema:TVSeries",
-    "schema:name": "Series Name",
+    "schema:name": "tvseries-14a4c8.mkv",
+    "schema:alternateName": "Series Name",
     "schema:actor": "Main Actor",
     "schema:numberOfSeasons": 3,
     "schema:contentSize": "356782930",
@@ -66,7 +70,8 @@ export const templates = {
     },
     "@type": "dweb:WebSite",
     "@id": "ant://1fac0b4e342802a4aff9a43a7427f0ac989475d4a5a6ff566445366b344cd063",
-    "schema:name": "AutoBoy",
+    "schema:name": "autoboy-website-72bf92.html",
+    "schema:alternateName": "AutoBoy",
     "schema:description": "safemedia Nintendo Gameboy Games Site"
   },
 
@@ -77,7 +82,8 @@ export const templates = {
     },
     "@type": "autonomi:directory",
     "@id": "ant://dcb90722cd6c7a3c66527fd8401970cad21cfc61f17e37abd421414ca26900f6",
-    "schema:name": "Richard Feynman Books",
+    "schema:name": "feynman-books-d8aa2f.dir",
+    "schema:alternateName": "Richard Feynman Books",
     "schema:contentSize": "41683410",
     "schema:description": "A collection of books by Richard Feynman",
     "schema:author": "Richard Feynman"
@@ -86,7 +92,8 @@ export const templates = {
   CreativeWork: {
     "@context": { "schema": "http://schema.org/" },
     "@type": "schema:CreativeWork",
-    "schema:name": "Example Creative Work",
+    "schema:name": "creativework-3a6f11.pdf",
+    "schema:alternateName": "Example Creative Work",
     "schema:creator": "Creator Name",
     "schema:description": "A brief description of the creative work.",
     "schema:contentSize": "15892",
@@ -100,7 +107,8 @@ export const templates = {
   AudioObject: {
     "@context": { "schema": "http://schema.org/" },
     "@type": "schema:AudioObject",
-    "schema:name": "Audio Clip",
+    "schema:name": "audio-clip-99c0df.mp3",
+    "schema:alternateName": "Audio Clip",
     "schema:contentUrl": "https://example.com/audio.mp3",
     "schema:encodingFormat": "audio/mpeg",
     "schema:contentSize": "720304",
@@ -114,7 +122,8 @@ export const templates = {
   ImageObject: {
     "@context": { "schema": "http://schema.org/" },
     "@type": "schema:ImageObject",
-    "schema:name": "Sample Image",
+    "schema:name": "sample-image-5e0102.jpg",
+    "schema:alternateName": "Sample Image",
     "schema:contentUrl": "https://example.com/image.jpg",
     "schema:encodingFormat": "image/jpeg",
     "schema:contentSize": "204832",
@@ -127,7 +136,8 @@ export const templates = {
   VideoObject: {
     "@context": { "schema": "http://schema.org/" },
     "@type": "schema:VideoObject",
-    "schema:name": "Sample Video",
+    "schema:name": "video-sample-6c5121.mp4",
+    "schema:alternateName": "Sample Video",
     "schema:contentUrl": "https://example.com/video.mp4",
     "schema:uploadDate": "2023-01-01",
     "schema:contentSize": "345190238",
@@ -141,7 +151,8 @@ export const templates = {
   MediaObject: {
     "@context": { "schema": "http://schema.org/" },
     "@type": "schema:MediaObject",
-    "schema:name": "Media Example",
+    "schema:name": "media-example-dede38.bin",
+    "schema:alternateName": "Media Example",
     "schema:contentUrl": "https://example.com/media",
     "schema:description": "A media object such as an image, video, or audio file.",
     "schema:contentSize": "8844132",
@@ -155,11 +166,11 @@ export const templates = {
     "@context": { "schema": "http://schema.org/" },
     "@type": "schema:SoftwareApplication",
     "@id": "ant://afea54f05bbc0e1369857fe23babfdff6a8d5894cd36a46d2e51151027d45f39",
-    "schema:name": "colony-x86_64-unknown-linux-musl v0.2.3",
+    "schema:name": "colony-x86_64-linux.iso",
+    "schema:alternateName": "colony-x86_64-unknown-linux-musl v0.2.3",
     "schema:description": "Colony CLI v0.2.3 x86_64 Linux binary. Using colonylib=v0.4.3 and autonomi=v0.5.0.",
     "schema:operatingSystem": "Linux",
     "schema:contentSize": "8000000",
     "schema:applicationCategory": "Application"
-  },
-
+  }
 };
