498b65d15c18e87d50579eee15e5ad689e24d74d
Chuck McClish <github.vanish213@passmail.net>
2025-07-19 12:57:04 -0400

    Moved the File Management sidebar menu back to the left hand side of the screen

--- src/components/drawer.svelte
+++ src/components/drawer.svelte
@@ -4,11 +4,11 @@
 
 <div class="drawer drawer-open" style="height: 100%; display: flex; flex-direction: row;">
   <input id={drawerId} type="checkbox" class="drawer-toggle" />
-  <div class="drawer-content flex flex-col" style="flex: 1; min-height: 0; overflow: hidden;">
-    <slot name="main" />
-  </div>
   <div class="drawer-side" style="height: 100%; flex-shrink: 0;">
     <label for={drawerId} aria-label="close sidebar" class="drawer-overlay"></label>
     <slot name="sidebar" />
+  </div>
+  <div class="drawer-content flex flex-col" style="flex: 1; min-height: 0; overflow: hidden;">
+    <slot name="main" />
   </div>
 </div>
