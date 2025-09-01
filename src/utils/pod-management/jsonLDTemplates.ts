80527e522da591ec43d0cf6fa8495fef01dc5dd3
Chuck McClish <huntedbysparrows@gmail.com>
2025-09-01 12:52:27 -0400

    Updated the default templates values to avoid conflicts

--- src/utils/pod-management/jsonLDTemplates.ts
+++ src/utils/pod-management/jsonLDTemplates.ts
@@ -5,8 +5,8 @@ export const templates = {
     "schema:name": "book-4fa2ad.pdf",
     "schema:alternateName": "Example Book Title",
     "schema:author": "Author Name",
-    "schema:contentSize": "563214",
-    "schema:datePublished": "2020",
+    "schema:contentSize": "5632141234",
+    "schema:datePublished": "2020-01-01 example date",
     "schema:description": "A sample book description.",
     "schema:encodingFormat": "application/pdf",
     "schema:inLanguage": "eng",
@@ -22,11 +22,11 @@ export const templates = {
     "schema:alternateName": "Example Movie Title",
     "schema:director": "Director Name",
     "schema:actor": "Lead Actor",
-    "schema:contentSize": "785412934",
-    "schema:datePublished": "2023-01-01",
+    "schema:contentSize": "7854129312344",
+    "schema:datePublished": "2023-01-01 example date",
     "schema:description": "A sample movie description.",
-    "schema:encodingFormat": "video/mp4",
-    "schema:inLanguage": "eng",
+    "schema:encodingFormat": "video/mp4 EXAMPLE",
+    "schema:inLanguage": "eng EXAMPLE",
     "schema:keywords": "Movie, Example",
     "schema:image": "https://example.com/movie.jpg"
   },
@@ -38,12 +38,12 @@ export const templates = {
     "schema:alternateName": "Track Title",
     "schema:byArtist": "Artist Name",
     "schema:inAlbum": "Album Name",
-    "schema:contentSize": "4159823",
-    "schema:datePublished": "2022-07-10",
+    "schema:contentSize": "41598231234",
+    "schema:datePublished": "2022-07-10 example date",
     "schema:description": "A music recording example.",
-    "schema:encodingFormat": "audio/mpeg",
-    "schema:inLanguage": "eng",
-    "schema:keywords": "Music, Track",
+    "schema:encodingFormat": "audio/mpeg EXAMPLE",
+    "schema:inLanguage": "eng EXAMPLE",
+    "schema:keywords": "Music, Track, Example",
     "schema:image": "https://example.com/track.jpg"
   },
 
@@ -53,13 +53,13 @@ export const templates = {
     "schema:name": "tvseries-14a4c8.mkv",
     "schema:alternateName": "Series Name",
     "schema:actor": "Main Actor",
-    "schema:numberOfSeasons": 3,
-    "schema:contentSize": "356782930",
-    "schema:datePublished": "2018",
+    "schema:numberOfSeasons": 400,
+    "schema:contentSize": "3567829301234",
+    "schema:datePublished": "2018 example date",
     "schema:description": "A TV series example.",
-    "schema:encodingFormat": "video/mp4",
-    "schema:inLanguage": "eng",
-    "schema:keywords": "TV, Series",
+    "schema:encodingFormat": "video/mp4 EXAMPLE",
+    "schema:inLanguage": "eng EXAMPLE",
+    "schema:keywords": "TV, Series, Example",
     "schema:image": "https://example.com/tvseries.jpg"
   },
 
@@ -70,9 +70,21 @@ export const templates = {
     },
     "@type": "dweb:WebSite",
     "@id": "ant://1fac0b4e342802a4aff9a43a7427f0ac989475d4a5a6ff566445366b344cd063",
-    "schema:name": "autoboy-website-72bf92.html",
-    "schema:alternateName": "AutoBoy",
-    "schema:description": "safemedia Nintendo Gameboy Games Site"
+    "schema:name": "website-72bf92.html",
+    "schema:alternateName": "Example Website Name",
+    "schema:description": "Example website description"
+  },
+
+  AntTp: {
+    "@context": {
+      "schema": "http://schema.org/",
+      "dweb": "ant://anttp/v1/"
+    },
+    "@type": "anttp:WebSite",
+    "@id": "ant://1fac0b4e342802a4aff9a43a7427f0ac989475d4a5a6ff566445366b344cd063",
+    "schema:name": "website-72bf92.html",
+    "schema:alternateName": "Example Website Name",
+    "schema:description": "Example website description"
   },
 
   AutonomiDirectory: {
@@ -82,11 +94,11 @@ export const templates = {
     },
     "@type": "autonomi:directory",
     "@id": "ant://dcb90722cd6c7a3c66527fd8401970cad21cfc61f17e37abd421414ca26900f6",
-    "schema:name": "feynman-books-d8aa2f.dir",
-    "schema:alternateName": "Richard Feynman Books",
-    "schema:contentSize": "41683410",
-    "schema:description": "A collection of books by Richard Feynman",
-    "schema:author": "Richard Feynman"
+    "schema:name": "directory_name",
+    "schema:alternateName": "Example Human Readable Name",
+    "schema:contentSize": "416834101234",
+    "schema:description": "Example description",
+    "schema:author": "Author"
   },
 
   CreativeWork: {
@@ -96,11 +108,11 @@ export const templates = {
     "schema:alternateName": "Example Creative Work",
     "schema:creator": "Creator Name",
     "schema:description": "A brief description of the creative work.",
-    "schema:contentSize": "15892",
-    "schema:encodingFormat": "application/pdf",
-    "schema:inLanguage": "eng",
-    "schema:keywords": "Sample, CreativeWork",
-    "schema:datePublished": "2023",
+    "schema:contentSize": "158921234",
+    "schema:encodingFormat": "application/pdf EXAMPLE",
+    "schema:inLanguage": "eng EXAMPLE",
+    "schema:keywords": "Sample, CreativeWork, Example",
+    "schema:datePublished": "2023 example date",
     "schema:image": "https://example.com/creativework.jpg"
   },
 
@@ -109,12 +121,11 @@ export const templates = {
     "@type": "schema:AudioObject",
     "schema:name": "audio-clip-99c0df.mp3",
     "schema:alternateName": "Audio Clip",
-    "schema:contentUrl": "https://example.com/audio.mp3",
-    "schema:encodingFormat": "audio/mpeg",
-    "schema:contentSize": "720304",
-    "schema:datePublished": "2023-02-14",
+    "schema:encodingFormat": "audio/mpeg EXAMPLE",
+    "schema:contentSize": "7203041234",
+    "schema:datePublished": "2023-02-14 example date",
     "schema:description": "An audio object example.",
-    "schema:inLanguage": "eng",
+    "schema:inLanguage": "eng EXAMPLE",
     "schema:keywords": "Audio, Example",
     "schema:image": "https://example.com/audio.jpg"
   },
@@ -124,12 +135,11 @@ export const templates = {
     "@type": "schema:ImageObject",
     "schema:name": "sample-image-5e0102.jpg",
     "schema:alternateName": "Sample Image",
-    "schema:contentUrl": "https://example.com/image.jpg",
-    "schema:encodingFormat": "image/jpeg",
-    "schema:contentSize": "204832",
-    "schema:datePublished": "2022-10-01",
+    "schema:encodingFormat": "image/jpeg EXAMPLE",
+    "schema:contentSize": "2048321234",
+    "schema:datePublished": "2022-10-01 example date",
     "schema:description": "An image object example.",
-    "schema:inLanguage": "eng",
+    "schema:inLanguage": "eng EXAMPLE",
     "schema:keywords": "Image, Example"
   },
 
@@ -138,12 +148,11 @@ export const templates = {
     "@type": "schema:VideoObject",
     "schema:name": "video-sample-6c5121.mp4",
     "schema:alternateName": "Sample Video",
-    "schema:contentUrl": "https://example.com/video.mp4",
-    "schema:uploadDate": "2023-01-01",
-    "schema:contentSize": "345190238",
-    "schema:encodingFormat": "video/mp4",
+    "schema:uploadDate": "2023-01-01 EXAMPLE",
+    "schema:contentSize": "3451902381234",
+    "schema:encodingFormat": "video/mp4 EXAMPLE",
     "schema:description": "A video object example.",
-    "schema:inLanguage": "eng",
+    "schema:inLanguage": "eng EXAMPLE",
     "schema:keywords": "Video, Example",
     "schema:image": "https://example.com/video.jpg"
   },
@@ -153,11 +162,10 @@ export const templates = {
     "@type": "schema:MediaObject",
     "schema:name": "media-example-dede38.bin",
     "schema:alternateName": "Media Example",
-    "schema:contentUrl": "https://example.com/media",
     "schema:description": "A media object such as an image, video, or audio file.",
-    "schema:contentSize": "8844132",
-    "schema:encodingFormat": "application/octet-stream",
-    "schema:inLanguage": "eng",
+    "schema:contentSize": "88441321234",
+    "schema:encodingFormat": "application/octet-stream EXAMPLE",
+    "schema:inLanguage": "eng EXAMPLE",
     "schema:keywords": "Media, Example",
     "schema:image": "https://example.com/media.jpg"
   },
@@ -167,18 +175,18 @@ export const templates = {
     "@type": "schema:SoftwareApplication",
     "@id": "ant://afea54f05bbc0e1369857fe23babfdff6a8d5894cd36a46d2e51151027d45f39",
     "schema:name": "colony-x86_64-linux.iso",
-    "schema:alternateName": "colony-x86_64-unknown-linux-musl v0.2.3",
-    "schema:description": "Colony CLI v0.2.3 x86_64 Linux binary. Using colonylib=v0.4.3 and autonomi=v0.5.0.",
-    "schema:operatingSystem": "Linux",
-    "schema:contentSize": "8000000",
-    "schema:applicationCategory": "Application"
+    "schema:alternateName": "Human Readble Name EXAMPLE",
+    "schema:description": "Description of the application. EXAMPLE",
+    "schema:operatingSystem": "Linux EXAMPLE",
+    "schema:contentSize": "80000001234",
+    "schema:applicationCategory": "Application EXAMPLE"
   },
   Simple: {
     "@context": { "schema": "http://schema.org/" },
     "@type": "schema:CreativeWork",
-    "schema:name": "creativework-3a6f11.pdf",
-    "schema:alternateName": "",
-    "schema:description": "",
-    "schem:comment": ""
+    "schema:name": "creativework-3a6f11.pdf EXAMPLE",
+    "schema:alternateName": "Human Readble Name EXAMPLE",
+    "schema:description": "Description of the file. EXAMPLE",
+    "schem:comment": "Comment about the file. EXAMPLE"
   },
 };
